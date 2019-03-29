893f9dd6bd08fa28ed860f7365edbb52b1076ee6
weicheng <weicheng@bilibili.com>
2019-03-29 11:48:40 +0800

    init

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+check:
+	python setup.py check
 test:
 	python -m unittest discover tests
+
+.PHONY: check test
--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name='discovery-client',
     version='0.1',
+    author='tomwei7',
+    url='https://github.com/tomwei7/discovery-client',
+    author_email='tomwei7g@gmail.com',
+    description='python client for discovery a registry for resilient mid-tier load balancing and failover. https://github.com/bilibili/discovery',
+    python_requires='>=3.5',
     packages=find_packages(exclude=["tests"]),
 )
